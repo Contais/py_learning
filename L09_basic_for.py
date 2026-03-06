41bf8c1f6a982d586dbe85633c8bb59d3b3fad90
lham <523800546@qq.com>
2026-03-06 20:46:08 +0800

    add: 在循环体使用关键字continue、break

--- L09_basic_for.py
+++ L09_basic_for.py
@@ -19,3 +19,20 @@ for i in range(1, 10):
 	for j in range (1, i + 1):
 		print(f"{j} x {i} = {i * j}", end = "\t")
 	print()
+
+
+# 猜数字游戏
+import random
+print("====== 猜数字游戏 ======")
+random_num = random.randint(1, 100)
+while True:
+	num = int(input("请输入一个数字(1-100)："))
+	if random_num > num :
+		print("猜小了~")
+		continue
+	elif random_num < num:
+		print("猜大了~")
+		continue
+	else:
+		print("恭喜猜对了~")
+		break
